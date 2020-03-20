8af792be10198ecee2e9613ad82d22b1e58d9506
Ryan MacInnes <ryan.macinnes@gmail.com>
2020-03-20 12:00:05 -0700

    Setup basic wrapper for js lib.

---
+++ src/components/embed.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+import { embedTweeker } from "tweeker-js"
+
+class Embed extends Component {
+  componentDidMount () {
+    const { businessId, embedVersion } = this.props
+
+    embedTweeker(businessId, embedVersion)
+  }
+
+  render() {
+    return null
+  }
+}
+
+Embed.defaultProps = {
+  businessId: null,
+  embedVersion: 'latest',
+}
+
+Embed.propTypes = {
+  businessId: PropTypes.string.isRequired,
+  embedVersion: PropTypes.string.isRequired,
+}
+
+export default Embed
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import TweekerEmbed from './components/embed'
+
+export {
+  TweekerEmbed
+}
